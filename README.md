40998b3cb331a45aad5a4b85f4c78b24a23bc006
miczed <mighty.miczed@gmail.com>
2017-05-18 13:22:04 +0200

    Added mock data and description to readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 Social-Computing-Twitter-Parser
+
+#Setup
+
+You need [composer](https://getcomposer.org/doc/00-intro.md) in order to install the dependencies for the projects. You also need a twitter developer account and valid keys.
+
+1. Run `composer install` in the directory to install all the directories.
+2. Create a `keys.php` file and insert your twitter API keys.
+
+````
+<?php
+define('API_CONSUMER_KEY','your-valid-key');
+define('API_CONSUMER_SECRET','your-valid-key');
+define('API_ACCESS_TOKEN','your-valid-key');
+define('API_TOKEN_SECRET','your-valid-key');
+````
--- index.php
+++ index.php
@@ -11,9 +11,24 @@ require_once "keys.php";
 
 use Abraham\TwitterOAuth\TwitterOAuth;
 
+// don't do more than 15 calls per 15 minutes (1 call every 1 minute should be enough)
+
 $connection = new TwitterOAuth(API_CONSUMER_KEY, API_CONSUMER_SECRET,API_ACCESS_TOKEN,API_TOKEN_SECRET );
 $tweets = $connection->get("statuses/user_timeline", ["screen_name" => "realDonaldTrump", "count" => 1, "trim_user" => true]);
 
 //$user = $connection->get("users/show", ["screen_name" => "realDonaldTrump"]);
 
 print_r($tweets);
+
+//writeToCSV("data/users.csv",$tweets);
+
+
+// Creates the file if it doesn't exists and writes to the end of it.
+function writeToCSV($url,$dataArray) {
+    $file = fopen($url,"a");
+
+    foreach($dataArray as $line) {
+        fputcsv($file,$line);
+    }
+    fclose($file);
+}
