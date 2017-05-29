8da2ca695ccf15b8bb3f66029c5b7cc7a56a1d46
miczed <mighty.miczed@gmail.com>
2017-05-29 09:17:03 +0200

    Added sql files

--- r/mysql.R
+++ r/mysql.R
@@ -3,9 +3,8 @@
 # into R data frames (and call them crimedat and pundat)
 
 library(DBI)
-myconn <-dbConnect(RMySQL::MySQL(),dbname='social-computing', username='root', password="root",host='127.0.0.0',port=3306)
-dbListTables(myconn)
-crimedat <- dbFetch(myconn, "users")
-pundat <- dbSendQuery(myconn, "select * from users")
-pundat
+myconn <-dbConnect(RMySQL::MySQL(),dbname='social-computing', username='root', password="root",host='localhost',port=3306)
+res <- dbGetQuery(myconn, "SELECT * FROM users JOIN (SELECT twitter_user_id,AVG(score * magnitude) as i FROM tweets_analyzed WHERE twitter_lang = 'en' GROUP BY twitter_user_id) t ON users.twitter_user_id = t.twitter_user_id ORDER BY i DESC")
 close(myconn)
+
+hist(res$i,breaks=12, col="red",xlab="Sentiment Index (score * magnitude)", main="Histogram of Sentiment Index")
